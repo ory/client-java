2ccf76594eeb294e6d0cff9fd4e605444902218d
aeneasr <3372410+aeneasr@users.noreply.github.com>
2025-12-01 14:59:35 +0000

    autogen: regenerate OpenAPI client for v1.22.13
    Version: v1.22.13

--- src/main/java/sh/ory/ServerVariable.java
+++ src/main/java/sh/ory/ServerVariable.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 /**
  * Representing a Server Variable for server URL template substitution.
  */
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2025-11-26T12:57:05.172785739Z[Etc/UTC]", comments = "Generator version: 7.7.0")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2025-12-01T14:47:54.071221266Z[Etc/UTC]", comments = "Generator version: 7.7.0")
 public class ServerVariable {
     public String description;
     public String defaultValue;
