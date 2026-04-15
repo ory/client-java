dbfdd88fbdce763e6cd7c6d2048c232f0dd6a1a7
aeneasr <3372410+aeneasr@users.noreply.github.com>
2026-04-15 07:49:07 +0000

    autogen: regenerate OpenAPI client for v1.22.35
    Version: v1.22.35

--- src/main/java/sh/ory/ServerConfiguration.java
+++ src/main/java/sh/ory/ServerConfiguration.java
@@ -5,7 +5,7 @@ import java.util.Map;
 /**
  * Representing a Server configuration.
  */
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-04-02T09:34:35.042951231Z[Etc/UTC]", comments = "Generator version: 7.7.0")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-04-15T07:38:07.718748626Z[Etc/UTC]", comments = "Generator version: 7.7.0")
 public class ServerConfiguration {
     public String URL;
     public String description;
